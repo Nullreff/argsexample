6b8ef76f8ac5ebefbb05b409edab3b9ea9e34ce2
Ryan Mendivil <ryan@nullreff.net>
2015-03-13 11:07:56 -0700

    Add -g flag to rustc options

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ cd build
 
 gcc -c ../printargs.c -o printargs.o -fPIC
 ar rcs libprintargs.a printargs.o
-rustc ../main.rs -L ./
+rustc -g ../main.rs -L ./
 
 ./main arg1 arg2 arg3
 ./main --arg1 arg2 arg3
